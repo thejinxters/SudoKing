22b58b2fb5126a8e997e114ea37112cf3dd5bf9c
Russell Teabeault <rteabeault@twitter.com>
2018-11-12 10:14:22 -0700

    Initial Commit

---
+++ SudoLikeAnOP/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SudoLikeAnOP
+//
+//  Created by Russell Teabeault on 11/12/18.
+//  Copyright © 2018 Russell Teabeault. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
